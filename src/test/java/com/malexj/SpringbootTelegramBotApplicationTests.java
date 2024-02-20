815af3aa5f76a8e750c17ceabb2a1e409ae3338d
malexj <alexmaximovj@gmail.com>
2024-02-20 15:47:57 +0200

    test docker file

--- src/test/java/com/malexj/SpringbootTelegramBotApplicationTests.java
+++ src/test/java/com/malexj/SpringbootTelegramBotApplicationTests.java
@@ -1,9 +1,10 @@
 package com.malexj;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = WavefrontProperties.Application.class)
 class SpringbootTelegramBotApplicationTests {
 
     @Test
